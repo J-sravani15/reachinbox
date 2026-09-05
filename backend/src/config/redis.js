fdb46966f940895729dcfe8ba8cca8d9c67b0045
J-sravani15 <sravanijagarlamudi2@gmail.com>
2026-09-05 21:09:37 +0530

    disable worker

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,19 +1,16 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisConnection = void 0;
-const ioredis_1 = __importDefault(require("ioredis"));
-exports.redisConnection = new ioredis_1.default({
-    host: "localhost",
-    port: 6379,
+import IORedis from "ioredis";
+
+export const redisConnection = new IORedis(
+  process.env.REDIS_URL!,
+  {
     maxRetriesPerRequest: null,
+  }
+);
+
+redisConnection.on("connect", () => {
+  console.log("✅ Redis Connected");
 });
-exports.redisConnection.on("connect", () => {
-    console.log("✅ Redis Connected");
+
+redisConnection.on("error", (err) => {
+  console.error("❌ Redis Error:", err);
 });
-exports.redisConnection.on("error", (err) => {
-    console.error("❌ Redis Error:", err);
-});
-//# sourceMappingURL=redis.js.map
